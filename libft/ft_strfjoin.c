40a0df8a7fbdbd7c299c0f09178e6ef1ca73cc76
Olivier ESPION <q.espion@gmail.com>
2018-04-30 11:23:59 +0200

    lub

---
+++ libft/ft_strfjoin.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+char    *ft_strfjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		len;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	if (!(str = (char*)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	ft_bzero(str, len + 1);
+	ft_strcat(str, s1);
+	free((char*)s1);
+	ft_strcat(str, s2);
+	return (str);
+}
