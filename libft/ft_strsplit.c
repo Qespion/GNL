cc3ad81956a43ee7e5d8b1c618403d0b7b9b6cff
Olivier ESPION <q.espion@gmail.com>
2018-05-01 14:42:54 +0200

    strsplit change

--- libft/ft_strsplit.c
+++ libft/ft_strsplit.c
@@ -6,12 +6,11 @@
 /*   By: oespion <oespion@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/04 11:22:37 by oespion           #+#    #+#             */
-/*   Updated: 2018/04/23 15:51:48 by oespion          ###   ########.fr       */
+/*   Updated: 2018/05/01 14:42:35 by oespion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 static int		ft_cw(char const *s, char c)
 {
